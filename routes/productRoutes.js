1f0ef9c8b090ee67a0e0879abc826a556f13b9ce
Artenlaclase <artenlaclase@gmail.com>
2024-09-08 17:51:39 -0300

    rutas productos

---
+++ routes/productRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { getAllProducts, createProduct, getProductById, updateProductById, deleteProductById } = require('../controllers/productControler');
+
+const productRouter = express.Router();
+
+productRouter.get('/readall', getAllProducts);
+
+productRouter.post('/create', createProduct);
+
+productRouter.get('/readone/:id', getProductById);
+
+productRouter.put('/update/:id', updateProductById);
+
+productRouter.delete('/delete/:id', deleteProductById)
+
+
+module.exports = productRouter;
