f5ec5be0eb9a2a17048abd176aec9d3ad25cfa6c
Artenlaclase <artenlaclase@gmail.com>
2024-09-08 11:04:13 -0300

    modelo productos

---
+++ models/productModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const productSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number
+    },
+    descripcion: {
+        type: String,
+        required: true
+    },
+    imagen:{
+        type: String
+    }
+
+},
+    {
+        timestamps: true
+    }
+)
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product;
+exports.Product = Product;
